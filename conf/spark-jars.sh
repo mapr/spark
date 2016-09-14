0e8e0ee4bf7c5c0fe81752df19afdd29823cb830
Rostyslav Sotnychenko <rsotnychenko@maprtech.com>
2016-09-14 13:46:30 +0300

    Minor fix for previous commit

--- conf/spark-jars.sh
+++ conf/spark-jars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SPARK_INSTALLATION_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SPARK_JARS_CLASSPATH="$SPARK_INSTALLATION_DIRECTORY/lib/spark-assembly-*.jar"
+SPARK_INSTALLATION_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+SPARK_JARS_CLASSPATH=$(ls ${SPARK_INSTALLATION_DIRECTORY}/lib/spark-assembly-*.jar)
