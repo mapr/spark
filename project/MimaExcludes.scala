d8f86576adfab98d91d2a4e0b1ace7881d815b2f
gatorsmile <gatorsmile@gmail.com>
2016-12-21 13:55:40 -0800

    [SPARK-18949][SQL][BACKPORT-2.1] Add recoverPartitions API to Catalog
    This PR is to backport https://github.com/apache/spark/pull/16356 to Spark 2.1.1 branch.
    
    ----
    
    Currently, we only have a SQL interface for recovering all the partitions in the directory of a table and update the catalog. `MSCK REPAIR TABLE` or `ALTER TABLE table RECOVER PARTITIONS`. (Actually, very hard for me to remember `MSCK` and have no clue what it means)
    
    fter the new "Scalable Partition Handling", the table repair becomes much more important for making visible the data in the created data source partitioned table.
    
    Thus, this PR is to add it into the Catalog interface. After this PR, users can repair the table by
    ```Scala
    spark.catalog.recoverPartitions("testTable")
    ```
    
    Modified the existing test cases.
    
    Author: gatorsmile <gatorsmile@gmail.com>
    
    Closes #16372 from gatorsmile/repairTable2.1.1.
    
    Signed-off-by: Rostyslav Sotnychenko <rsotnychenko@maprtech.com>

--- project/MimaExcludes.scala
+++ project/MimaExcludes.scala
@@ -115,6 +115,8 @@ object MimaExcludes {
       ProblemFilters.exclude[IncompatibleMethTypeProblem]("org.apache.spark.sql.streaming.StreamingQueryException.this"),
       ProblemFilters.exclude[DirectMissingMethodProblem]("org.apache.spark.sql.streaming.StreamingQueryException.query"),
 
+      // [SPARK-18949] [SQL] Add repairTable API to Catalog
+      ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.sql.catalog.Catalog.recoverPartitions"),
       // [SPARK-19267] Fetch Failure handling robust to user error handling
       ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.TaskContext.setFetchFailed")
     )
