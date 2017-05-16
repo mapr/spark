148167b6b592d38335ca47ef2093fcf7d5a84749
Rostyslav Sotnychenko <rsotnychenko@maprtech.com>
2017-05-16 13:08:42 +0300

    [MAPR-26414] Spark History Server Memory Leak fixed (#112)

--- core/src/main/scala/org/apache/spark/scheduler/EventLoggingListener.scala
+++ core/src/main/scala/org/apache/spark/scheduler/EventLoggingListener.scala
@@ -310,8 +310,10 @@ private[spark] object EventLoggingListener extends Logging {
       }
 
       codec.map(_.compressedInputStream(in)).getOrElse(in)
-    } finally {
-      in.close()
+    } catch {
+      case e: Exception =>
+        in.close()
+        throw e
     }
   }
 
