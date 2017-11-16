989550e6fc0cd540583568370b1edd20ee1df30e
Mikhail Gorbov <mgorbov@mapr.com>
2017-11-16 14:25:03 +0200

    Spark Mapr-DB connector was refactored according to Scala style Removed code duplication.

--- external/maprdb/src/main/scala/com/mapr/db/spark/utils/MapRDBUtils.scala
+++ external/maprdb/src/main/scala/com/mapr/db/spark/utils/MapRDBUtils.scala
@@ -17,30 +17,35 @@ import org.ojai.Document
 import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 
-
 private[spark] object MapRDBUtils {
-  def checkOrCreateTable(tableName: String, bulkMode: Boolean, createTable: Boolean, keys : Seq[org.ojai.Value]): (Boolean, Boolean) = {
+  def checkOrCreateTable(tableName: String,
+                         bulkMode: Boolean,
+                         createTable: Boolean,
+                         keys: Seq[org.ojai.Value]): (Boolean, Boolean) = {
     var isBulkLoad: Boolean = bulkMode
     if (!DBClient().tableExists(tableName)) {
-      if (!createTable) throw new TableNotFoundException("Table: "+ tableName + " not found")
+      if (!createTable) {
+        throw new TableNotFoundException("Table: " + tableName + " not found")
+      }
+
       val tabDesc = DBClient().newTableDescriptor()
       tabDesc.setAutoSplit(true)
       tabDesc.setPath(tableName)
       tabDesc.setBulkLoad(bulkMode)
       tabDesc.setInsertionOrder(false)
-      if (keys.isEmpty)
+      if (keys.isEmpty) {
         DBClient().createTable(tabDesc)
-      else
+      } else {
         DBClient().createTable(tabDesc, keys.toArray)
-      return (true,bulkMode)
-    } else if (createTable ) {
-      throw new TableExistsException("Table: " + tableName + " already Exists")
-    }
+      }
 
-    if (bulkMode == true) {
-      isBulkLoad = DBClient().isBulkLoad(tableName)
+      (true, bulkMode)
+    } else if (createTable) {
+      throw new TableExistsException("Table: " + tableName + " already Exists")
+    } else {
+      if (bulkMode == true) isBulkLoad = DBClient().isBulkLoad(tableName)
+      (false, isBulkLoad)
     }
-    return (false, isBulkLoad)
   }
 
   def setBulkLoad(tableName: String, bulkMode: Boolean): Unit = {
@@ -49,20 +54,23 @@ private[spark] object MapRDBUtils {
     DBClient().alterTable(desc)
   }
 
-  def containsRow(row : Array[Byte], rowkeyRange: RowkeyRange): Boolean = {
-    return Bytes.compareTo(row, rowkeyRange.getStartRow) >= 0 &&
+  def containsRow(row: Array[Byte], rowkeyRange: RowkeyRange): Boolean = {
+    Bytes.compareTo(row, rowkeyRange.getStartRow) >= 0 &&
       (Bytes.compareTo(row, rowkeyRange.getStopRow) < 0 ||
-        Bytes.equals(rowkeyRange.getStopRow, MapRConstants.EMPTY_BYTE_ARRAY));
+        Bytes.equals(rowkeyRange.getStopRow, MapRConstants.EMPTY_BYTE_ARRAY))
   }
 
   def toBeanClass[T](doc: Document, beanClass: Class[T]): T = {
-    if (beanClass.getSimpleName.equals("OJAIDocument"))
+    if (beanClass.getSimpleName.equals("OJAIDocument")) {
       MapRDBSpark.newDocument(doc).asInstanceOf[T]
-    else
-      BeanCodec.encode[T](doc.asReader(),beanClass)
+    } else {
+      BeanCodec.encode[T](doc.asReader(), beanClass)
+    }
   }
 
-  def readBytes(buff: ByteBuffer, bufferSize: Int, objectInput : ObjectInput): Unit = {
+  def readBytes(buff: ByteBuffer,
+                bufferSize: Int,
+                objectInput: ObjectInput): Unit = {
     val byteArray = new Array[Byte](bufferSize)
     var readbytes = objectInput.read(byteArray, 0, bufferSize)
     buff.put(byteArray, 0, readbytes)
@@ -85,11 +93,13 @@ private[spark] object MapRDBUtils {
     }
   }
 
-  def convertToMap(value : Map[String,Any]): Map[String, AnyRef] = {
+  def convertToMap(value: Map[String, Any]): Map[String, AnyRef] = {
     if (value.isInstanceOf[DBMapValue]) {
-      value.asInstanceOf[DBMapValue].value.map{ case (k,v) => k -> v.asInstanceOf[AnyRef]}
+      value.asInstanceOf[DBMapValue].value.map {
+        case (k, v) => k -> v.asInstanceOf[AnyRef]
+      }
     } else {
-      value.map{ case (k,v) => k -> convertToScalaCollection(v)}
+      value.map { case (k, v) => k -> convertToScalaCollection(v) }
     }
   }
 
@@ -99,27 +109,41 @@ private[spark] object MapRDBUtils {
     } else if (value.isInstanceOf[DBArrayValue[_]]) {
       return value.asInstanceOf[DBArrayValue[_]].arr.asJava.asInstanceOf[AnyRef]
     } else if (value.isInstanceOf[Map[_, _]]) {
-      return value.asInstanceOf[Map[String,Any]].map{case (k,v) => k -> convertToScalaCollection(v)}.asJava.asInstanceOf[AnyRef]
+      return value
+        .asInstanceOf[Map[String, Any]]
+        .map { case (k, v) => k -> convertToScalaCollection(v) }
+        .asJava
+        .asInstanceOf[AnyRef]
     } else if (value.isInstanceOf[Seq[Any]]) {
-      return value.asInstanceOf[Seq[Any]].map(convertToScalaCollection(_)).asJava.asInstanceOf[AnyRef]
+      return value
+        .asInstanceOf[Seq[Any]]
+        .map(convertToScalaCollection(_))
+        .asJava
+        .asInstanceOf[AnyRef]
     } else {
       return value.asInstanceOf[AnyRef]
     }
   }
 
-  def getOjaiKey[T : ClassTag]() : OJAIKey[T] = {
+  def getOjaiKey[T: ClassTag](): OJAIKey[T] = {
     import reflect._
     val result = classTag[T] match {
-      case a if a <<: classTag[String] => return OJAIKey.ojaiStringKey.asInstanceOf[OJAIKey[T]]
-      case a if a <<: classTag[ByteBuffer] => return OJAIKey.ojaibytebufferKey.asInstanceOf[OJAIKey[T]]
-      case a if a <<: classTag[DBBinaryValue] => return OJAIKey.ojaibinaryKey.asInstanceOf[OJAIKey[T]]
-      case _ => throw new RuntimeException("Key with type:" + classTag[T].runtimeClass + " is not supported")
+      case a if a <<: classTag[String] =>
+        return OJAIKey.ojaiStringKey.asInstanceOf[OJAIKey[T]]
+      case a if a <<: classTag[ByteBuffer] =>
+        return OJAIKey.ojaibytebufferKey.asInstanceOf[OJAIKey[T]]
+      case a if a <<: classTag[DBBinaryValue] =>
+        return OJAIKey.ojaibinaryKey.asInstanceOf[OJAIKey[T]]
+      case _ =>
+        throw new RuntimeException(
+          "Key with type:" + classTag[T].runtimeClass + " is not supported")
     }
 
     return result.asInstanceOf[OJAIKey[T]]
   }
 
   implicit class ClassTagOps[T](val classTag: ClassTag[T]) extends AnyVal {
-    def <<:(other: ClassTag[_]) = classTag.runtimeClass.isAssignableFrom(other.runtimeClass)
+    def <<:(other: ClassTag[_]): Boolean =
+      classTag.runtimeClass.isAssignableFrom(other.runtimeClass)
   }
 }
