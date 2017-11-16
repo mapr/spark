7eeca7d3107f4c3b46420bc92e9c4a043faa3270
Mikhail Gorbov <mgorbov@mapr.com>
2017-11-16 14:25:03 +0200

    Spark Mapr-DB connector was refactored according to Scala style Removed code duplication

--- external/maprdb/src/main/scala/com/mapr/db/spark/documentUtils/ScalaDocumentIterator.scala
+++ external/maprdb/src/main/scala/com/mapr/db/spark/documentUtils/ScalaDocumentIterator.scala
@@ -1,52 +1,52 @@
 /* Copyright (c) 2015 & onwards. MapR Tech, Inc., All rights reserved */
 package com.mapr.db.spark.documentUtils
 
-import org.ojai.Value
+import java.util
 import java.util.Map
 
 import com.mapr.db.spark.codec.BeanCodec
 import com.mapr.db.spark.impl.OJAIDocument
 import org.ojai.Document
+import org.ojai.Value
 
 /*
  * This class is a bridge between the java Map Iterator and scala Map Iterator.
  * It enables users to iterate over java Map.
  */
-class ScalaDocumentIterator(val iter: java.util.Iterator[Map.Entry[String, Value]])
-  extends Iterator[Tuple2[String,Value]] {
+class ScalaDocumentIterator(val iter: java.util.Iterator[util.Map.Entry[String, Value]])
+    extends Iterator[(String, Value)] {
 
-  def hasNext = iter.hasNext
+  def hasNext: Boolean = iter.hasNext
 
-  def next() = {
+  def next: (String, Value) = {
     val elem = iter.next()
-    (elem.getKey -> elem.getValue)
+    elem.getKey -> elem.getValue
   }
 }
 
+class OJAIDocumentIterator(val iter: java.util.Iterator[Document])
+    extends Iterator[OJAIDocument] {
 
-class OJAIDocumentIterator(val iter: java.util.Iterator[Document]) extends Iterator[OJAIDocument] {
+  def hasNext: Boolean = iter.hasNext
 
-  def hasNext = iter.hasNext
-
-  def next() = {
+  def next: OJAIDocument = {
     new OJAIDocument(iter.next())
   }
 }
 
-class BeanIterator[T](val iter: java.util.Iterator[Document], val beanClass: Class[T]) extends Iterator[T] {
+class BeanIterator[T](val iter: java.util.Iterator[Document], val beanClass: Class[T])
+    extends Iterator[T] {
 
-  def hasNext = iter.hasNext
+  def hasNext: Boolean = iter.hasNext
 
-  def next() = {
-    BeanCodec.encode[T](iter.next.asReader(), beanClass)
-  }
+  def next: T = BeanCodec.encode[T](iter.next.asReader(), beanClass)
+
 }
 
-case class JavaBeanIterator[T](val iter: java.util.Iterator[Document], val beanClass: Class[T]) extends Iterator[T] {
+case class JavaBeanIterator[T](iter: java.util.Iterator[Document], beanClass: Class[T])
+    extends Iterator[T] {
 
-  def hasNext = iter.hasNext
+  def hasNext: Boolean = iter.hasNext
 
-  def next() = {
-    org.ojai.beans.BeanCodec.encode[T](iter.next.asReader(), beanClass)
-  }
+  def next: T = org.ojai.beans.BeanCodec.encode[T](iter.next.asReader(), beanClass)
 }
