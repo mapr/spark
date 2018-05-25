8e5e575516ce2b54db37d9728502db87db7e23f0
Egor Krivokon <ekrivokon@mapr.com>
2018-05-25 16:42:31 +0300

    Spark 226 (#277)
    * MapR [SPARK-226] Spark - pySpark Security Vulnerability

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -3275,11 +3275,12 @@ class Dataset[T] private[sql](
   /**
    * Collect a Dataset as Arrow batches and serve stream to PySpark.
    */
+
   private[sql] def collectAsArrowToPython(): Array[Any] = {
     val timeZoneId = sparkSession.sessionState.conf.sessionLocalTimeZone
-
-    withAction("collectAsArrowToPython", queryExecution) { plan =>
-      PythonRDD.serveToStream("serve-Arrow") { out =>
+    withNewExecutionId {
+      val iter = toArrowPayload.collect().iterator.map(_.asPythonSerializable)
+      PythonRDD.serveIterator(iter, "serve-Arrow") { out =>
         val batchWriter = new ArrowBatchStreamWriter(schema, out, timeZoneId)
         val arrowBatchRdd = toArrowBatchRdd(plan)
         val numPartitions = arrowBatchRdd.partitions.length
