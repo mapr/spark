55195ae84bf30d68f1ffa8b26e3898273e4da320
Egor Krivokon <33221486+ekrivokonmapr@users.noreply.github.com>
2018-08-30 17:25:42 +0300

    MapR [SPARK-279] Can't connect to spark thrift server with new spark and hive packages (#348)

--- sql/hive-thriftserver/src/main/java/org/apache/hive/service/auth/HiveAuthFactory.java
+++ sql/hive-thriftserver/src/main/java/org/apache/hive/service/auth/HiveAuthFactory.java
@@ -19,14 +19,19 @@ package org.apache.hive.service.auth;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.*;
 import javax.security.auth.login.LoginException;
 import javax.security.sasl.Sasl;
 
@@ -236,6 +241,47 @@ public class HiveAuthFactory {
     params.setTrustStore(trustStorePath, trustStorePassWord);
     params.requireClientAuth(true);
     return TSSLTransportFactory.getClientSocket(host, port, loginTimeout, params);
+  }
+
+  //Create SSL Socket for MAPRSASL connection. Ignore SSL trusted servers as MAPRSASL perform encryption by itself
+  public static TTransport getTrustAllSSLSocket(String host, int port, int loginTimeout) throws TTransportException {
+    TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509ExtendedTrustManager() {
+              @Override
+              public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+              }
+              @Override
+              public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+              }
+              @Override
+              public X509Certificate[] getAcceptedIssuers() {
+                return null;
+              }
+              @Override
+              public void checkClientTrusted(X509Certificate[] x509Certificates, String s, Socket socket) throws CertificateException {
+              }
+              @Override
+              public void checkServerTrusted(X509Certificate[] x509Certificates, String s, Socket socket) throws CertificateException {
+              }
+              @Override
+              public void checkClientTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine) throws CertificateException {
+              }
+              @Override
+              public void checkServerTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine) throws CertificateException {
+              }
+            }
+    };
+    SSLSocket socket;
+    try {
+      SSLContext sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+      SSLSocketFactory factory = sslContext.getSocketFactory();
+      socket = (SSLSocket) factory.createSocket(host, port);
+      socket.setSoTimeout(loginTimeout);
+    } catch (NoSuchAlgorithmException | IOException | KeyManagementException e) {
+      throw new TTransportException("Couldn't create Trust All SSL socket", e);
+    }
+    return new TSocket(socket);
   }
 
   public static TServerSocket getServerSocket(String hiveHost, int portNum)
