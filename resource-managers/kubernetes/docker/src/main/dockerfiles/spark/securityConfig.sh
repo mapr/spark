2477228725eacfd9c1dfa245e34c13afbed0d072
Oleksandr Mukhopad <alex.mukhopad@gmail.com>
2019-03-01 18:22:12 +0200

    MapR [SPARK-486][K8S] Fix sasl encryption error on Kubernetes

--- resource-managers/kubernetes/docker/src/main/dockerfiles/spark/securityConfig.sh
+++ resource-managers/kubernetes/docker/src/main/dockerfiles/spark/securityConfig.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+function validateUserCredentials() {
+    set +x
+    MAPR_SPARK_USER=${MAPR_SPARK_USER:-mapr}
+    MAPR_SPARK_GROUP=${MAPR_SPARK_GROUP:-mapr}
+    MAPR_SPARK_PASSWORD=${MAPR_SPARK_PASSWORD:-mapr}
+    MAPR_SPARK_UID=${MAPR_SPARK_UID:-5000}
+    MAPR_SPARK_GID=${MAPR_SPARK_GID:-5000}
+    set -x
+}
 
 function createUserGroups() {
   groups=($MAPR_SPARK_GROUP)
@@ -33,27 +42,49 @@ function copySecurity() {
   fi
 }
 
-function configurePod() {
-    createUser
-    createUserGroups
-    copySecurity
-
+function configureSecurity() {
     MAPR_USER=${MAPR_SPARK_USER}
     MAPR_GROUP=${MAPR_SPARK_GROUP}
 
-    if [ "$SECURE_CLUSTER" == "true" ] ; then
-        /opt/mapr/server/configure.sh -c -C $MAPR_CLDB_HOSTS -Z $MAPR_ZK_HOSTS -N $MAPR_CLUSTER -secure
-        ${SPARK_HOME}/bin/configure.sh -c --secure
+    if [ "$SECURE_CLUSTER" == "true" ]; then
+         MAPR_SECURE="-secure"
     else
-        /opt/mapr/server/configure.sh -c -C $MAPR_CLDB_HOSTS -Z $MAPR_ZK_HOSTS -N $MAPR_CLUSTER
+         MAPR_SECURE="-unsecure"
     fi
+
+    /opt/mapr/server/configure.sh -c -C $MAPR_CLDB_HOSTS -Z $MAPR_ZK_HOSTS -N $MAPR_CLUSTER -genkeys "${MAPR_SECURE}"
+    ${SPARK_HOME}/bin/configure.sh -c "-${MAPR_SECURE}"
+}
+
+function configureK8SProperties() {
+    cat >> $SPARK_CONF_PATH <<EOM
+spark.authenticate false
+spark.authenticate.enableSaslEncryption false
+spark.io.encryption.enabled     false
+spark.hadoop.yarn.resourcemanager.ha.custom-ha-enabled  false
+spark.hadoop.yarn.resourcemanager.recovery.enabled      false
+EOM
 
     # copy k8s submit properties to spark-defaults
     defaultK8SConfMount=/opt/spark/conf/spark.properties
     if [ -f ${defaultK8SConfMount} ]; then
         cat ${defaultK8SConfMount} >> $SPARK_CONF_PATH
     fi
+}
 
-    #Configure logger info level
-    sed 's/rootCategory=WARN/rootCategory=INFO/' "$SPARK_CONF_DIR/log4j.properties"
+function configureLogs() {
+    log4jConf=$SPARK_CONF_DIR/log4j.properties
+    if [ -f ${log4jConf} ]; then
+        sed 's/rootCategory=WARN/rootCategory=INFO/' ${log4jConf}
+    fi
+}
+
+function configurePod() {
+    validateUserCredentials
+    createUser
+    createUserGroups
+    copySecurity
+    configureSecurity
+    configureK8SProperties
+    configureLogs
 }
