0bb97c75e37f10645ff0fb5cb17e343c9f3601bd
Mikhail Gorbov <mgorbov@maprtech.com>
2019-04-04 19:05:15 +0300

    MapR [SPARK-516] Spark jobs failure using yarn mode on kerberos fixed (#491)

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -362,6 +362,7 @@ private[spark] class Executor(
               }
             }
             val proxyUgi = UserGroupInformation.createProxyUser(doAsUserName, ugi)
+            proxyUgi.addCredentials(ugi.getCredentials)
             proxyUgi.doAs(doAsAction)
           }
         } finally {
