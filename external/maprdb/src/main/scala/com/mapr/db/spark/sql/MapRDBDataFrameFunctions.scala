0478c3acf1407d42c780f9dc4163ac8f86f074c7
Nicolas A Perez <anicolaspp@gmail.com>
2019-06-20 10:56:13 +0300

    Adding `joinWithMapRDBTable` function (#529)
    The related documentation of this function is here https://github.com/anicolaspp/MapRDBConnector#joinwithmaprdbtable.
    
    The main idea is that having a dataframe (no matter how was it constructed) we can join it with a MapR-DB table. This functions looks at the join query and load only those records from MapR-DB that will join instead of loading the full table and then join in memory. In other words, we only load what we know will be joint.

--- external/maprdb/src/main/scala/com/mapr/db/spark/sql/MapRDBDataFrameFunctions.scala
+++ external/maprdb/src/main/scala/com/mapr/db/spark/sql/MapRDBDataFrameFunctions.scala
@@ -1,14 +1,16 @@
 /* Copyright (c) 2015 & onwards. MapR Tech, Inc., All rights reserved */
 package com.mapr.db.spark.sql
 
+import com.mapr.db.spark.sql.ojai.{JoinType, OJAISparkPartitionReader}
+import com.mapr.db.spark.sql.ojai.OJAISparkPartitionReader.Cell
 import com.mapr.db.spark.utils.{LoggingTrait, MapRSpark}
 import org.ojai.DocumentConstants
-
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types.StructType
 
 private[spark] case class MapRDBDataFrameFunctions(@transient df: DataFrame,
                                                    bufferWrites: Boolean = true)
-    extends LoggingTrait {
+  extends LoggingTrait {
 
   def setBufferWrites(bufferWrites: Boolean): MapRDBDataFrameFunctions =
     MapRDBDataFrameFunctions(df, bufferWrites)
@@ -24,4 +26,44 @@ private[spark] case class MapRDBDataFrameFunctions(@transient df: DataFrame,
                      createTable: Boolean = false,
                      bulkInsert: Boolean = false): Unit =
     MapRSpark.insert(df, tableName, idFieldPath, createTable, bulkInsert, bufferWrites)
+
+  def joinWithMapRDBTable(table: String,
+                          schema: StructType,
+                          left: String,
+                          right: String,
+                          joinType: JoinType,
+                          concurrentQueries: Int = 20)(implicit session: SparkSession): DataFrame = {
+
+    val columnDataType = schema.fields(schema.fieldIndex(right)).dataType
+
+    val documents = df
+      .select(left)
+      .distinct()
+      .rdd
+      .mapPartitions { partition =>
+        if (partition.isEmpty) {
+          List.empty.iterator
+        } else {
+
+          val partitionCellIterator = partition.map(row => Cell(row.get(0), columnDataType))
+
+          OJAISparkPartitionReader
+            .groupedPartitionReader(concurrentQueries)
+            .readFrom(partitionCellIterator, table, schema, right)
+        }
+      }
+
+    import org.apache.spark.sql.functions._
+    import session.implicits._
+
+    val rightDF = session.read.schema(schema).json(documents.toDS())
+
+    df.join(rightDF, col(left) === col(right), joinType.toString)
+  }
+
+  def joinWithMapRDBTable(maprdbTable: String,
+                          schema: StructType,
+                          left: String,
+                          right: String)(implicit session: SparkSession): DataFrame =
+    joinWithMapRDBTable(maprdbTable, schema, left, right, JoinType.inner)
 }
