c2183d6b6db6b0ec5de59dad041f01869d1246cc
Rostyslav Sotnychenko <rsotnychenko@mapr.com>
2019-07-10 21:50:36 +0300

    SPARK-579 info about ssl_trustore is added for metrics

--- core/src/main/scala/org/apache/spark/ui/SparkMetricsUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/SparkMetricsUtils.scala
@@ -16,18 +16,32 @@ private[spark] object SparkMetricsUtils extends SparkMetricsUtils with Logging {
 
   private val sparkMetricsZkRoot = "/spark-metrics"
   private val metricsZkConf = "spark.metrics.zookeeper.url"
+  private val sslTrustoreLocation = System.getProperty("user.home") + "/spark/security_keys/ssl_truststore"
 
-  def dumpMetricsURLToZookeeper(appId : String, url : String, port: Int, sparkConf: SparkConf): Option[CuratorFramework] = {
-    if(port == -1) {
+  def dumpMetricsURLToZookeeper(appId : String,
+                                url : String,
+                                boundPort: Int,
+                                securePort: Option[Int],
+                                sparkConf: SparkConf): Option[CuratorFramework] = {
+    if(boundPort == -1) {
       logWarning(s"Cannot create metrics znode for unbound app: $appId")
       // No need to create znode for unbounded application
       return None
     }
 
+    val sslUiEnabled = !securePort.isEmpty
+
     val fqdn = InetAddress.getLocalHost.getCanonicalHostName
-    val data = s"$port,$url"
+    val data = constractZNodeData(sslUiEnabled,
+                                  boundPort,
+                                  securePort,
+                                  url,
+                                  appId) match {
+      case Some(data) => data
+      case _ => return None
+    }
     val subFolder = s"$sparkMetricsZkRoot/$fqdn"
-    val node = s"$subFolder/$port"
+    val node = s"$subFolder/$boundPort"
 
     val zkURLs = getZookeeperURLs
     sparkConf.set(metricsZkConf, zkURLs)
@@ -39,6 +53,28 @@ private[spark] object SparkMetricsUtils extends SparkMetricsUtils with Logging {
     zk.create.withProtectedEphemeralSequential.forPath(node, data.getBytes)
 
     Some(zk)
+  }
+
+  private def constractZNodeData(sslUiEnabled: Boolean,
+                                 boundPort: Int,
+                                 securePort: Option[Int],
+                                 url: String,
+                                 appId : String): Option[String] = {
+    if (sslUiEnabled) {
+      val pattern = "(http://)(.*):(\\d+)".r
+      val secureUrl = url match {
+        case pattern(_, fqdn, _) => s"https://$fqdn:${securePort.get}"
+        case _ => {
+          logWarning(s"Base url does not match the pattern: url=$url, pattern=$pattern . " +
+          s"Cannot create metrics znode for app: $appId")
+          return None
+        }
+      }
+      Some(s"${securePort.get},$secureUrl,$sslTrustoreLocation")
+    }
+    else {
+      Some(s"$boundPort,$url")
+    }
   }
 
   private def getZookeeperURLs(): String = {
