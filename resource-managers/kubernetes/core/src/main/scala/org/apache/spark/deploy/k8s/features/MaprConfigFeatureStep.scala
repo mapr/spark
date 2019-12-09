323bdad0575690b754f3611c0809794e579232af
Alexandr Navara <alexandr.navara@gmail.com>
2019-12-09 22:10:13 +0200

    K8S-1077 (#598)
    * K8S-1077 - use single k8s secret with user info

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/features/MaprConfigFeatureStep.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/features/MaprConfigFeatureStep.scala
@@ -1,11 +1,11 @@
 package org.apache.spark.deploy.k8s.features
 
 import scala.collection.JavaConverters._
-import io.fabric8.kubernetes.api.model.{ContainerBuilder, EnvVarBuilder, HasMetadata}
+import io.fabric8.kubernetes.api.model.{ContainerBuilder, EnvVarBuilder, HasMetadata, PodBuilder, VolumeBuilder}
 import org.apache.spark.SparkConf
+import org.apache.spark.deploy.k8s.{KubernetesConf, KubernetesRoleSpecificConf, SparkPod}
 import org.apache.spark.deploy.k8s.Config._
 import org.apache.spark.deploy.k8s.Constants._
-import org.apache.spark.deploy.k8s.{KubernetesConf, KubernetesRoleSpecificConf, SparkPod}
 
 private[spark] class MaprConfigFeatureStep(
     conf: KubernetesConf[_ <: KubernetesRoleSpecificConf])
@@ -22,37 +22,34 @@ private[spark] class MaprConfigFeatureStep(
           .build()
       }
 
-    val maprTicketSecret =
-      s"$KUBERNETES_DRIVER_SECRETS_PREFIX${sparkConf.get(MAPR_TICKET_SECRET_PREFIX)}"
-
-    val maprTicketEnv = sparkConf
-      .getAllWithPrefix(maprTicketSecret).toSeq
-      .map { case (_, value) =>
-        new EnvVarBuilder()
-          .withName(MAPR_TICKETFILE_LOCATION)
-          .withValue(value + s"/${sparkConf.get(MAPR_TICKET_SECRET_KEY)}")
-          .build()
-      }
-
-    val maprSslSecret =
-      s"$KUBERNETES_DRIVER_SECRETS_PREFIX${sparkConf.get(MAPR_SSL_SECRET_PREFIX)}"
-
-    val maprSslEnv = sparkConf
-      .getAllWithPrefix(maprSslSecret).toSeq
-      .map { case (_, value) =>
-        new EnvVarBuilder()
-          .withName(MAPR_SSL_LOCATION)
-          .withValue(value)
-          .build()
-      }
-
     val clusterConfMap = sparkConf.get(MAPR_CLUSTER_CONFIGMAP).toString
-    val clusterUserSecrets = sparkConf.get(MAPR_CLUSTER_USER_SECRETS).toString
+    val userSecret = sparkConf.get(MAPR_USER_SECRET).toString
+    val userSecretVolumeName = s"$userSecret-volume"
+    val userSecretMountPath = "/tmp/maprticket"
+    val ticketFileLocation = s"$userSecretMountPath/${sparkConf.get(MAPR_TICKET_SECRET_KEY)}"
 
-    val container = new ContainerBuilder(pod.container)
+    val maprPod = new PodBuilder(pod.pod)
+      .editOrNewSpec()
+      .addToVolumes(
+        new VolumeBuilder()
+          .withName(userSecretVolumeName)
+          .withNewSecret()
+            .withSecretName(userSecret)
+          .endSecret()
+          .build())
+      .endSpec()
+      .build()
+
+    val maprContainer = new ContainerBuilder(pod.container)
       .addAllToEnv(clusterEnvs.asJava)
-      .addAllToEnv(maprTicketEnv.asJava)
-      .addAllToEnv(maprSslEnv.asJava)
+      .addNewEnv()
+        .withName(MAPR_TICKETFILE_LOCATION)
+        .withValue(ticketFileLocation)
+      .endEnv()
+      .addNewVolumeMount()
+        .withName(userSecretVolumeName)
+        .withMountPath(userSecretMountPath)
+      .endVolumeMount()
       .addNewEnvFrom()
         .withNewConfigMapRef()
           .withName(clusterConfMap)
@@ -60,12 +57,12 @@ private[spark] class MaprConfigFeatureStep(
         .endEnvFrom()
       .addNewEnvFrom()
         .withNewSecretRef()
-          .withName(clusterUserSecrets)
+          .withName(userSecret)
           .endSecretRef()
         .endEnvFrom()
       .build()
 
-    SparkPod(pod.pod, container)
+    SparkPod(maprPod, maprContainer)
   }
 
   override def getAdditionalPodSystemProperties(): Map[String, String] = Map.empty
