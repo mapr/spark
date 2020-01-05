fc41916441e8e02c6b368d30b46f44ab45531873
Egor Krivokon <>
2020-01-05 19:00:18 +0200

    Back default Spark branch to 2.4.4-mapr

--- core/src/main/java/org/apache/spark/ui/filters/CustomHeadersFilter.java
+++ core/src/main/java/org/apache/spark/ui/filters/CustomHeadersFilter.java
@@ -45,8 +45,7 @@ public class CustomHeadersFilter implements Filter {
 
     private void initDefaultHeaderProperties() {
         DEFAULT_HEADER_PROPERTIES.put("Strict-Transport-Security", "max-age=31536000;includeSubDomains");
-        //    TODO: Move Content-Security-Policy header back after fix for the COMMSECURE-130
-        //    DEFAULT_HEADER_PROPERTIES.put("Content-Security-Policy", "default-src https:");
+        DEFAULT_HEADER_PROPERTIES.put("Content-Security-Policy", "default-src https:");
         DEFAULT_HEADER_PROPERTIES.put("X-Content-Type-Options", "nosniff");
         DEFAULT_HEADER_PROPERTIES.put("X-XSS-Protection", "1; mode=block");
     }
