b4076fb96bad3f26bfade59299884bcf60638d99
Nicolas A Perez <anicolaspp@gmail.com>
2020-02-11 23:32:50 +0200

    Support for Java Types Filtering
    - When writing application in Java, they types are not being pushed down since the Java types are not the same as Scala Types

---
+++ external/maprdb/src/main/scala/com/mapr/db/spark/sql/v2/SupportedFilterTypes.scala
@@ -0,0 +1,28 @@
+package com.mapr.db.spark.sql.v2
+
+import java.sql.Timestamp
+
+object SupportedFilterTypes {
+
+  private lazy val supportedTypes = List[Class[_]](
+    classOf[Double],
+    classOf[java.lang.Double],
+    classOf[Float],
+    classOf[java.lang.Float],
+    classOf[Int],
+    classOf[Integer],
+    classOf[Long],
+    classOf[java.lang.Long],
+    classOf[Short],
+    classOf[java.lang.Short],
+    classOf[String],
+    classOf[Timestamp],
+    classOf[Boolean],
+    classOf[java.lang.Boolean],
+    classOf[Byte],
+    classOf[java.lang.Byte]
+  )
+
+  def isSupportedType(value: Any): Boolean = supportedTypes.contains(value.getClass)
+}
+
