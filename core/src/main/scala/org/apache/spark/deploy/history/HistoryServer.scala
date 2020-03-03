905ac3d24821d8dccf58896b6f151dba5f46bdfc
Egor Krivokon <>
2020-03-03 18:37:43 +0200

    MapR [SPARK-677] No TLS protocol logging for Spark thrift server & spark history server

--- core/src/main/scala/org/apache/spark/deploy/history/HistoryServer.scala
+++ core/src/main/scala/org/apache/spark/deploy/history/HistoryServer.scala
@@ -327,7 +327,8 @@ object HistoryServer extends Logging {
     new HistoryServerArguments(conf, argStrings)
     initSecurity()
     val securityManager = createSecurityManager(conf)
-    logInfo(s"Using SSL protocol: ${securityManager.getSSLOptions("protocol").protocol.get}")
+    logInfo(s"Using SSL protocol: " +
+      s"${securityManager.getSSLOptions("protocol").protocol.getOrElse("None")}")
 
     val providerName = conf.get(History.PROVIDER)
       .getOrElse(classOf[FsHistoryProvider].getName())
