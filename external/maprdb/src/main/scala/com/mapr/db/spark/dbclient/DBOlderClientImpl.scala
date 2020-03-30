b82699d07ef031cababa229e54418dc57e2b5788
Egor Krivokon <>
2020-03-30 18:29:01 +0300

    MapR [SPARK-695] Spark 2.4.5 porting to MapR

---
+++ external/maprdb/src/main/scala/com/mapr/db/spark/dbclient/DBOlderClientImpl.scala
@@ -0,0 +1,89 @@
+/* Copyright (c) 2015 & onwards. MapR Tech, Inc., All rights reserved */
+package com.mapr.db.spark.dbclient
+
+import com.mapr.db.{MapRDB, TableDescriptor}
+import com.mapr.db.impl.AdminImpl
+import com.mapr.db.scan.ScanRange
+import com.mapr.ojai.store.impl.OjaiDocumentStore
+
+import org.ojai.{Document, DocumentBuilder, Value}
+import org.ojai.store.{DocumentStore, DriverManager, QueryCondition}
+
+
+import scala.collection.JavaConverters._
+
+object DBOlderClientImpl extends DBClient {
+
+  private val connection = DriverManager.getConnection("ojai:mapr:")
+  private val driver = connection.getDriver
+  private val bufferWritesOption = "ojai.mapr.documentstore.buffer-writes"
+
+  override def newDocument(): Document = {
+    driver.newDocument()
+  }
+
+  override def getTabletInfos(tablePath: String, cond: QueryCondition,
+                              bufferWrites: Boolean): Seq[ScanRange] = {
+
+    connection.getStore(tablePath, connection.newDocument()
+      .set(bufferWritesOption, bufferWrites))
+      .asInstanceOf[OjaiDocumentStore].getTable.getMetaTable
+      .getScanRanges(cond).asScala
+  }
+
+  override def getTabletInfos(tablePath: String, bufferWrites: Boolean): Seq[ScanRange] = {
+
+      connection.getStore(tablePath, connection.newDocument()
+        .set(bufferWritesOption, bufferWrites))
+        .asInstanceOf[OjaiDocumentStore].getTable.getMetaTable
+        .getScanRanges().asScala
+  }
+
+  override def newDocument(jsonString: String): Document =
+    driver.newDocument(jsonString)
+
+  override def newCondition(): QueryCondition = driver.newCondition()
+
+  override def deleteTable(tablePath: String): Unit =
+    MapRDB.deleteTable(tablePath)
+
+  override def tableExists(tablePath: String): Boolean =
+    MapRDB.tableExists(tablePath)
+
+  override def newTableDescriptor(): TableDescriptor =
+    MapRDB.newTableDescriptor()
+
+  override def createTable(tablePath: String): Unit =
+    MapRDB.createTable(tablePath)
+
+  override def createTable(tableDesc: TableDescriptor): Unit =
+    MapRDB.newAdmin().createTable(tableDesc)
+
+  override def createTable(tableDesc: TableDescriptor, keys: Array[Value]): Unit =
+    MapRDB
+      .newAdmin()
+      .asInstanceOf[AdminImpl]
+      .createTable(tableDesc, keys)
+
+  override def isBulkLoad(tablePath: String): Boolean =
+    MapRDB
+      .newAdmin()
+      .getTableDescriptor(tablePath)
+      .isBulkLoad
+
+  override def alterTable(tableDesc: TableDescriptor): Unit =
+    MapRDB.newAdmin().alterTable(tableDesc)
+
+  override def getTable(tablePath: String, bufferWrites: Boolean): DocumentStore =
+    connection.getStore(tablePath, connection.newDocument()
+      .set(bufferWritesOption, bufferWrites))
+
+  override def getTableDescriptor(tablePath: String): TableDescriptor =
+    MapRDB.newAdmin().getTableDescriptor(tablePath)
+
+  override def getEstimatedSize(scanRange: ScanRange): Long = 0
+
+  override def newDocumentBuilder(): DocumentBuilder =
+    driver.newDocumentBuilder()
+
+}
