a741f48f16745fe170c462d5459b9d5c1f089173
Egor Krivokon <33221486+ekrivokonmapr@users.noreply.github.com>
2020-06-24 13:00:32 +0300

    MapR [SPARK-760] DataSourceV2 ClassCastException (#693)

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameReader.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameReader.scala
@@ -212,7 +212,7 @@ class DataFrameReader private[sql](sparkSession: SparkSession) extends Logging {
 
     val cls = DataSource.lookupDataSource(source, sparkSession.sessionState.conf)
     if (classOf[DataSourceV2].isAssignableFrom(cls)) {
-      val ds = cls.getConstructor().asInstanceOf[DataSourceV2]
+      val ds = cls.getConstructor().newInstance().asInstanceOf[DataSourceV2]
       if (ds.isInstanceOf[ReadSupport]) {
         val sessionOptions = DataSourceV2Utils.extractSessionConfigs(
           ds = ds, conf = sparkSession.sessionState.conf)
