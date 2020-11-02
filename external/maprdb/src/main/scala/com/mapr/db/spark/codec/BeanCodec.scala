06747a020ed0273620f87872056f6e74d96fcc34
Mikhail Gorbov <mgorbov@mapr.com>
2020-11-03 00:30:52 +0200

    MapR [SPARK-804] sparkContext.loadFromMapRDB throwing RuntimeException in scala code

---
+++ external/maprdb/src/main/scala/com/mapr/db/spark/codec/BeanCodec.scala
@@ -0,0 +1,39 @@
+/* Copyright (c) 2015 & onwards. MapR Tech, Inc., All rights reserved */
+package com.mapr.db.spark.codec
+
+import java.io.IOException
+
+import com.mapr.db.spark.codec.JacksonBeanCodecHelper._
+import org.ojai.{Document, DocumentBuilder, DocumentReader}
+import org.ojai.annotation.API
+import org.ojai.beans.jackson.{DocumentGenerator, DocumentParser}
+import org.ojai.exceptions.{DecodingException, EncodingException}
+
+@API.Internal
+object BeanCodec {
+  @throws[DecodingException]
+  def decode(db: DocumentBuilder, bean: Any): Document = {
+    if (bean == null) return null
+    val gen: DocumentGenerator = new DocumentGenerator(db)
+    try {
+      MAPPER.writeValue(gen, bean)
+      gen.getDocument
+    }
+    catch {
+      case e: Exception =>
+        throw new DecodingException("Failed to convert the java bean to Document", e)
+    }
+  }
+
+  @throws[EncodingException]
+  def encode[T](dr: DocumentReader, beanClass: Class[T]): T = {
+    if (dr == null) return null.asInstanceOf[T]
+    try {
+      MAPPER.readValue(new DocumentParserImpl(dr), beanClass)
+    }
+    catch {
+      case e: IOException =>
+        throw new EncodingException("Failed to create java bean from Document", e)
+    }
+  }
+}
---
+++ external/maprdb/src/main/scala/com/mapr/db/spark/codec/JacksonBeanCodecHelper.scala
@@ -0,0 +1,120 @@
+/* Copyright (c) 2015 & onwards. MapR Tech, Inc., All rights reserved */
+
+package com.mapr.db.spark.codec
+
+import java.io.IOException
+
+import com.fasterxml.jackson.core.{JsonGenerator, JsonParser, JsonProcessingException, Version}
+import com.fasterxml.jackson.core.util.VersionUtil
+import com.fasterxml.jackson.databind.{DeserializationContext, JsonDeserializer, JsonSerializer, ObjectMapper, SerializerProvider}
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import org.ojai.annotation.API
+import org.ojai.beans.jackson.JacksonHelper
+import org.ojai.types.{ODate, OInterval, OTime, OTimestamp}
+
+@API.Internal
+object JacksonBeanCodecHelper {
+
+  val MAPPER: ObjectMapper = new ObjectMapper
+
+  class ByteSerializer extends JsonSerializer[Byte] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def serialize(value: Byte, jgen: JsonGenerator, provider: SerializerProvider) {
+      jgen.writeObject(value)
+    }
+  }
+
+  class IntervalSerializer extends JsonSerializer[OInterval] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def serialize(value: OInterval, jgen: JsonGenerator, provider: SerializerProvider) {
+      jgen.writeObject(value)
+    }
+  }
+
+  class IntervalDeserializer extends JsonDeserializer[OInterval] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def deserialize(p: JsonParser, ctxt: DeserializationContext): OInterval = {
+      p.getEmbeddedObject.asInstanceOf[OInterval]
+    }
+  }
+
+  class DateSerializer extends JsonSerializer[ODate] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def serialize(value: ODate, jgen: JsonGenerator, provider: SerializerProvider) {
+      jgen.writeObject(value)
+    }
+  }
+
+  class DateDeserializer extends JsonDeserializer[ODate] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def deserialize(p: JsonParser, ctxt: DeserializationContext): ODate = {
+      p.getEmbeddedObject.asInstanceOf[ODate]
+    }
+  }
+
+  class TimeSerializer extends JsonSerializer[OTime] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def serialize(value: OTime, jgen: JsonGenerator, provider: SerializerProvider) {
+      jgen.writeObject(value)
+    }
+  }
+
+  class TimeDeserializer extends JsonDeserializer[OTime] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def deserialize(p: JsonParser, ctxt: DeserializationContext): OTime = {
+      p.getEmbeddedObject.asInstanceOf[OTime]
+    }
+  }
+
+  class TimestampSerializer extends JsonSerializer[OTimestamp] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def serialize(value: OTimestamp, jgen: JsonGenerator, provider: SerializerProvider) {
+      jgen.writeObject(value)
+    }
+  }
+
+  class TimestampDeserializer extends JsonDeserializer[OTimestamp] {
+    @throws[IOException]
+    @throws[JsonProcessingException]
+    def deserialize(p: JsonParser, ctxt: DeserializationContext): OTimestamp = {
+      p.getEmbeddedObject.asInstanceOf[OTimestamp]
+    }
+  }
+
+  val version: Version = {
+    val ver: String = classOf[JacksonHelper].getPackage.getImplementationVersion
+    if (ver == null) {
+      VersionUtil.parseVersion("<unknown>", "org.ojai", "core")
+    } else {
+      VersionUtil.parseVersion(ver, "org.ojai", "core")
+    }
+  }
+
+  val byteSerializer: JacksonBeanCodecHelper.ByteSerializer =
+    new JacksonBeanCodecHelper.ByteSerializer
+
+  val module: SimpleModule = new SimpleModule("OjaiSerializers", version)
+  module.addSerializer(classOf[Byte], byteSerializer)
+  module.addSerializer(classOf[OInterval], new JacksonHelper.IntervalSerializer)
+  module.addDeserializer(classOf[OInterval], new JacksonHelper.IntervalDeserializer)
+  module.addSerializer(classOf[ODate], new JacksonHelper.DateSerializer)
+  module.addDeserializer(classOf[ODate], new JacksonHelper.DateDeserializer)
+  module.addSerializer(classOf[OTime], new JacksonHelper.TimeSerializer)
+  module.addDeserializer(classOf[OTime], new JacksonHelper.TimeDeserializer)
+  module.addSerializer(classOf[OTimestamp], new JacksonHelper.TimestampSerializer)
+  module.addDeserializer(classOf[OTimestamp], new JacksonHelper.TimestampDeserializer)
+  MAPPER.registerModule(DefaultScalaModule)
+  MAPPER.registerModule(module)
+
+}
+
