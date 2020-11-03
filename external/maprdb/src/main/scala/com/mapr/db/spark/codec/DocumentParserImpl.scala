e59f9e6b03c789b4d10c31b69e4b187fdce0aa93
Mikhail Gorbov <mgorbov@mapr.com>
2020-11-04 00:22:37 +0200

    MapR [SPARK-804] sparkContext.loadFromMapRDB throwing RuntimeException in scala code

---
+++ external/maprdb/src/main/scala/com/mapr/db/spark/codec/DocumentParserImpl.scala
@@ -0,0 +1,17 @@
+/* Copyright (c) 2015 & onwards. MapR Tech, Inc., All rights reserved */
+
+package com.mapr.db.spark.codec
+
+import com.fasterxml.jackson.core.JsonLocation
+import org.ojai.DocumentReader
+import org.ojai.beans.jackson.DocumentParser
+
+class DocumentParserImpl(dr: DocumentReader) extends DocumentParser(dr) {
+  override def getCurrentLocation: JsonLocation = {
+    JsonLocation.NA
+  }
+
+  override def getTokenLocation: JsonLocation = {
+    JsonLocation.NA
+  }
+}
