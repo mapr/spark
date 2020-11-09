0c632a4e0352cbd130ab206364c7472a26c26ae2
Mikhail Gorbov <mgorbov@mapr.com>
2020-11-09 11:40:28 +0200

    Update Spark from 2.4.5 to 3.0.1

--- core/src/test/scala/org/apache/spark/deploy/history/HistoryServerDiskManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/history/HistoryServerDiskManagerSuite.scala
@@ -34,8 +34,6 @@ class HistoryServerDiskManagerSuite extends SparkFunSuite with BeforeAndAfter {
 
   private def doReturn(value: Any) = org.mockito.Mockito.doReturn(value, Seq.empty: _*)
 
-  private def doReturn(value: Any) = org.mockito.Mockito.doReturn(value, Seq.empty: _*)
-
   private val MAX_USAGE = 3L
 
   private var testDir: File = _
