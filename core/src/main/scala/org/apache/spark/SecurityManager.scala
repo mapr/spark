2bcc0c09f70b015e4d82bc92ae0e3b31abc7584f
Egor Krivokon <33221486+ekrivokonmapr@users.noreply.github.com>
2021-04-13 18:22:07 +0300

    MapR [SPARK-870] Can't download event logs from SHS twice (#805)

--- core/src/main/scala/org/apache/spark/SecurityManager.scala
+++ core/src/main/scala/org/apache/spark/SecurityManager.scala
@@ -172,7 +172,6 @@ private[spark] class SecurityManager(
         new Path(certGeneratorLog),
         new Path(certGeneratorLogMfsLocation)
       )
-      fs.close()
 
       if (res != 0) {
         throw new Exception(s"Failed to generate SSL certificates for spark WebUI. Exit code: $res" )
@@ -220,7 +219,6 @@ private[spark] class SecurityManager(
           new Path(s"$localBaseDir${fStringPath.substring(fStringPath.lastIndexOf("/"))}"))
       }
     }
-    fs.close()
   }
 
   private def updateSslOptsWithNewKeystore(sslOptions: SSLOptions,
