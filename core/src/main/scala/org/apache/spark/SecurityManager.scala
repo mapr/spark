fe3c37b2b7c40259fb6d07cb56656419ef89a698
Arsenii Venherak <57108150+avenherak@users.noreply.github.com>
2021-04-16 15:21:08 +0300

    MapR [SPARK-871] Spark 2.4.7 job fails from mapr-client (#807)

--- core/src/main/scala/org/apache/spark/SecurityManager.scala
+++ core/src/main/scala/org/apache/spark/SecurityManager.scala
@@ -158,7 +158,7 @@ private[spark] class SecurityManager(
       manageSslKeysLocalFile.setExecutable(true)
       val sslKeyStorePass = getSSLOptions("ui").keyStorePassword.get
 
-      val file = new File(certGeneratorLog)
+      val file = new File(s"${System.getProperty("user.home")}/$certGeneratorLog")
 
       val stdStream = new OutputStreamWriter(new FileOutputStream(file), UTF_8)
       val stdWriter = new PrintWriter(stdStream)
@@ -169,7 +169,7 @@ private[spark] class SecurityManager(
       fs.copyFromLocalFile(
         false,
         true,
-        new Path(certGeneratorLog),
+        new Path(file.getPath),
         new Path(certGeneratorLogMfsLocation)
       )
 
