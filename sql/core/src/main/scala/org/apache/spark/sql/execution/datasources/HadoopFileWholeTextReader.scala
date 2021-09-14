c6be19fd2d7649e0261b516f5c43c5ba8e155ebf
Andrew Khalymon <andrew.khalymon@hpe.com>
2021-09-14 14:09:58 +0300

    MapR [SPARK-941] Problem with count via spark-shell

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/HadoopFileWholeTextReader.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/HadoopFileWholeTextReader.scala
@@ -28,41 +28,43 @@ import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit
 import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl
 
 import org.apache.spark.input.WholeTextFileRecordReader
+import org.apache.spark.internal.Logging
 
 /**
  * An adaptor from a [[PartitionedFile]] to an [[Iterator]] of [[Text]], which is all of the lines
  * in that file.
  */
 class HadoopFileWholeTextReader(file: PartitionedFile, conf: Configuration)
-  extends Iterator[Text] with Closeable {
+  extends Iterator[Text] with Closeable with Logging{
+
+  private def getFileSplit(file : PartitionedFile, conf: Configuration): CombineFileSplit = {
+    try {
+      val filePathData = FileUtil.checkPathForSymlink(new Path(file.filePath), conf)
+      new CombineFileSplit(
+        Array(filePathData.path),
+        Array(file.start),
+        Array(filePathData.stat.getLen),
+        Array.empty[String])
+    } catch {
+      case _: NoSuchMethodError =>
+        log.warn("Symlink check failed. " +
+          "Please install latest mapr-patch to avoid this warning. " +
+          "See MAPR-SPARK-941 for ref")
+        new CombineFileSplit(
+          Array(new Path(new URI(file.filePath))),
+          Array(file.start),
+          Array(file.length),
+          Array.empty[String])
+    }
+  }
+
   private val iterator = {
-    val filePathData = FileUtil.checkPathForSymlink(new Path(file.filePath), conf)
-    val fileSplit = getFileSplit
+    val fileSplit = getFileSplit(file, conf)
     val attemptId = new TaskAttemptID(new TaskID(new JobID(), TaskType.MAP, 0), 0)
     val hadoopAttemptContext = new TaskAttemptContextImpl(conf, attemptId)
     val reader = new WholeTextFileRecordReader(fileSplit, hadoopAttemptContext, 0)
     reader.initialize(fileSplit, hadoopAttemptContext)
     new RecordReaderIterator(reader)
-  }
-
-  private def getFileSplit : CombineFileSplit = {
-    // TODO: Implement Locality
-    if (checkForSymlink) {
-      val filePathData = FileUtil.checkPathForSymlink(new Path(file.filePath), conf)
-      val fileSplit = new CombineFileSplit(
-        Array(filePathData.path),
-        Array(file.start),
-        Array(filePathData.stat.getLen),
-        Array.empty[String])
-      fileSplit
-    } else {
-      val fileSplit = new CombineFileSplit(
-        Array(new Path(new URI(file.filePath))),
-        Array(file.start),
-        Array(file.length),
-        Array.empty[String])
-      fileSplit
-    }
   }
 
   override def hasNext: Boolean = iterator.hasNext
