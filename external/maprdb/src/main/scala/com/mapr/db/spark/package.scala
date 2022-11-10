4414ccd755967a935fe2420ea5a7697a9b7057bd
Egor Krivokon <egor.krivokon@hpe.com>
2022-11-11 00:48:00 +0200

    Spark 3.3.1 porting

--- external/maprdb/src/main/scala/com/mapr/db/spark/package.scala
+++ external/maprdb/src/main/scala/com/mapr/db/spark/package.scala
@@ -11,8 +11,6 @@ import com.mapr.db.spark.writers.{OJAIKey, OJAIValue}
 import com.mapr.org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.SparkContext
 
-import scala.language.implicitConversions
-
 import org.apache.spark.rdd.RDD
 
 package object spark {
