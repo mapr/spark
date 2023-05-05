0596a9bd7b85e52432bd01fac87e5ea0d9fd26a6
Egor Krivokon <33221486+ekrivokonmapr@users.noreply.github.com>
2023-05-05 12:56:18 +0300

    Spark-3.4.0 porting

---
+++ connector/maprdb/src/main/scala/com/mapr/db/spark/sql/ojai/JoinType.scala
@@ -0,0 +1,34 @@
+package com.mapr.db.spark.sql.ojai
+
+sealed trait JoinType
+
+object JoinType {
+
+  def apply(value: String): JoinType = joins.indexWhere(_.toString == value.toLowerCase()) match {
+    case -1 => throw new IllegalArgumentException(s"$value is not a supported join type")
+    case idx => joins(idx)
+  }
+
+  private lazy val joins = List(inner, outer, full, left, left_outer)
+
+  case object inner extends JoinType {
+    override def toString: String = "inner"
+  }
+
+  case object outer extends JoinType {
+    override def toString: String = "outer"
+  }
+
+  case object full extends JoinType {
+    override def toString: String = "full"
+  }
+
+  case object left extends JoinType {
+    override def toString: String = "left"
+  }
+
+  case object left_outer extends JoinType {
+    override def toString: String = "left_outer"
+  }
+
+}
