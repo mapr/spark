372f6495e545ee06f20fbcbb423b7cd008d59695
Ivan Dzikovskyi <ivan.dzikovskyi@hpe.com>
2023-11-11 09:04:55 +0200

    MapR [DFDEVOPS-3022] Add an option to easily modify build images per component (#1153)
    Co-authored-by: Anatoliy Shevchuk <anatoliy.shevchuk@hpe.com>

---
+++ devops/buildimages/centos/image.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+BASE_IMAGE=${DOCKER_REGISTRY}/centos8-java11-gcc8
+BASE_TAG=latest
+
+BASEDIR=$(dirname $0)
+DOCKERFILE_CHECKSUM=$(md5sum "${BASEDIR}/Dockerfile" | head -c 7)
+
+IMAGE_NAME_FOR_BUILD=${BASE_IMAGE}-custom-${REPOSITORY_NAME}:${BASE_TAG}-${DOCKERFILE_CHECKSUM}
+
+ensure_image() {
+    docker image pull -q ${IMAGE_NAME_FOR_BUILD}
+    retVal=$?
+    if [ "$retVal" -ne 0 ]; then
+        docker build -q ${BASEDIR} -f ${BASEDIR}/Dockerfile -t ${IMAGE_NAME_FOR_BUILD} --build-arg BASE_IMAGE=${BASE_IMAGE}:${BASE_TAG}
+        docker push -q ${IMAGE_NAME_FOR_BUILD}
+    fi
+}
+
+ensure_image >&2
+
+echo ${IMAGE_NAME_FOR_BUILD}
