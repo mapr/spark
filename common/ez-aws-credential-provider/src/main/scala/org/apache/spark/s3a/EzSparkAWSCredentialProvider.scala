690a645d5dc6809ffc6640f9aef232780d36bc33
Egor Krivokon <33221486+ekrivokonmapr@users.noreply.github.com>
2025-02-26 16:33:16 +0200

    EZAF-7644: EzSparkAWSCredentialProvider does not refresh the token automatically (#1248)

--- common/ez-aws-credential-provider/src/main/scala/org/apache/spark/s3a/EzSparkAWSCredentialProvider.scala
+++ common/ez-aws-credential-provider/src/main/scala/org/apache/spark/s3a/EzSparkAWSCredentialProvider.scala
@@ -2,10 +2,11 @@ package org.apache.spark.s3a
 
 import com.amazonaws.AmazonClientException
 import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider, BasicAWSCredentials}
-
+import org.apache.commons.io.monitor.{FileAlterationListener, FileAlterationMonitor, FileAlterationObserver}
 import org.apache.hadoop.conf.Configuration
 import org.apache.spark.internal.Logging
 
+import java.io.File
 import java.net.URI
 import scala.io.Source._
 import scala.util.Properties._
@@ -19,6 +20,7 @@ class EzSparkAWSCredentialProvider(uri: URI, conf: Configuration) extends AWSCre
   private var secretKey = ""
 
   refresh()
+  startKeyUpdateMonitor()
 
   override def getCredentials: AWSCredentials = {
     new BasicAWSCredentials(accessKey, secretKey)
@@ -52,6 +54,42 @@ class EzSparkAWSCredentialProvider(uri: URI, conf: Configuration) extends AWSCre
       logInfo(s"Reading key from location: $keyLocation")
       val keySource = fromFile(keyLocation)
       try keySource.mkString.trim finally keySource.close()
+    }
+  }
+
+  private def startKeyUpdateMonitor(): Unit = {
+    val accessKeyLocation = envOrElse("AWS_ACCESS_KEY_LOCATION", DefaultAccessKeyLocation)
+    val accessKeyFile = new File(accessKeyLocation)
+
+    val observer = new FileAlterationObserver(accessKeyFile.getParent)
+
+    observer.addListener(new FileAlterationListener {
+      override def onFileChange(file: File): Unit = {
+        if (file.getName == accessKeyFile.getName) {
+          logInfo(s"Access key file changed: ${file.getName}")
+          refresh()
+        }
+      }
+      override def onFileCreate(file: File): Unit = {
+        if (file.getName == accessKeyFile.getName) {
+          logInfo(s"Access key file created: ${file.getName}")
+          refresh()
+        }
+      }
+      override def onFileDelete(file: File): Unit = {}
+      override def onDirectoryChange(directory: File): Unit = {}
+      override def onDirectoryCreate(directory: File): Unit = {}
+      override def onDirectoryDelete(directory: File): Unit = {}
+      override def onStart(observer: FileAlterationObserver): Unit = {}
+      override def onStop(observer: FileAlterationObserver): Unit = {}
+    })
+
+    val monitor = new FileAlterationMonitor(10000, observer)
+
+    try {
+      monitor.start()
+    } catch {
+      case e: Exception => logError("Error starting file monitor", e)
     }
   }
 }
